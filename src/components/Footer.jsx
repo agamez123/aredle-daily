062dfb95059860542a2d53ef32dd97d20ed97719
agamez <renbriggs10@gmail.com>
2026-08-09 10:37:35 -0700

    Add Footer + GD Icon

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css"
+
+function Footer() {
+    return(
+        <footer className="site-footer">
+            <span className="site-footer__cube" aria-hidden="true">
+                <span className="site-footer__cube-eye site-footer__cube-eye--left" />
+                <span className="site-footer__cube-eye site-footer__cube-eye--right" />
+                <span className="site-footer__cube-mouth" />
+            </span>
+            <span className="site-footer__line-mask site-footer__line-mask--cube" aria-hidden="true" />
+            <span className="site-footer__spike" aria-hidden="true" />
+            <span className="site-footer__line-mask site-footer__line-mask--spike" aria-hidden="true" />
+            <span>yo</span>
+        </footer>
+    )
+}
+
+export default Footer
